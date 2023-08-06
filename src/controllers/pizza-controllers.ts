c912607ed6b1c535280801f9078f466ddc9b2d85
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2023-08-06 21:33:09 +0200

    Added routes: getPizzaDetails, patchPizzaName, deletePizza

--- src/controllers/pizza-controllers.ts
+++ src/controllers/pizza-controllers.ts
@@ -6,8 +6,6 @@ import { Ingredient } from "../models/ingredient";
 export const addPizza = async (req: Request, res: Response) => {
     try {
         const { name, actions, ingredients } = req.body;
-        console.log("actions:", actions);
-        console.log("ingredients:", ingredients);
         const pizzaExist = await Pizza.findOne({ name });
 
         if (pizzaExist) {
@@ -38,4 +36,63 @@ export const addPizza = async (req: Request, res: Response) => {
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong during adding Pizza. Please, try again later.' });
     }
+}
+
+export const getPizzaDetails = async (req: Request, res: Response) => {
+    const pizzaName = req.params.pizzaName
+    const pizzaExist = await Pizza.findOne({ name: pizzaName })
+        .populate("ingredients", "name -_id")
+        .populate("actions", "name -_id");
+
+    res.status(200).json({ ingredients: pizzaExist?.ingredients, actions: pizzaExist?.actions });
+}
+
+export const patchPizzaName = async (req: Request, res: Response) => {
+    const pizzaName = req.params.pizzaName;
+    const data = {
+        name: req.body.name,
+    };
+
+    try {
+        const pizzaExist = await Pizza.findOne({ name: pizzaName })
+        if (!pizzaExist) {
+            return res.status(404).json({
+                success: false,
+                message: 'Pizza not found.',
+            });
+        };
+
+        await Pizza.findOneAndUpdate({ name: pizzaName }, data)
+
+        res.status(200).json({ success: true, message: 'Pizza name was updated.' });
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong while updating the pizza name. Please try again later.',
+        });
+    }
+}
+
+export const deletePizza = async (req: Request, res: Response) => {
+    const pizzaName = req.params.pizzaName;
+
+    try {
+        const pizzaExist = await Pizza.findOne({ name: pizzaName });
+        if (pizzaExist) {
+            await pizzaExist.deleteOne({ name: pizzaName });
+            res.status(204).json({ success: true, message: 'Pizza was deleted.' });
+
+            await Ingredient.updateMany({ pizzas: pizzaExist._id }, { $pull: { pizzas: pizzaExist._id } });
+            await Action.updateMany({ pizzas: pizzaExist._id }, { $pull: { pizzas: pizzaExist._id } });
+        } else {
+            res.status(404).json({ success: false, message: 'Pizza not found.' });
+        }
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong while deleting the pizza. Please try again later.',
+        });
+    }
+
 }
--- src/routes/pizza-routes.ts
+++ src/routes/pizza-routes.ts
@@ -1,8 +1,13 @@
-import { Router } from 'express';
-import { addPizza } from '../controllers/pizza-controllers';
+import { Request, Response, Router } from 'express';
+import { addPizza, deletePizza, getPizzaDetails, patchPizzaName } from '../controllers/pizza-controllers';
+import { Pizza } from '../models/pizza';
 
 const router = Router();
 
 router.post('/new', addPizza);
+router.get('/get/:pizzaName', getPizzaDetails);
+router.patch('/:pizzaName', patchPizzaName);
+router.delete('/:pizzaName', deletePizza)
+
 
 export default router
