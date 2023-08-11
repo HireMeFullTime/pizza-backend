d098417585ba7abf9b10133943d547f627d60a2f
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2023-08-11 13:10:12 +0200

    Added action validation

--- src/routes/action-routes.ts
+++ src/routes/action-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { addAction, deleteAction, getActionDetails, getAllActionsNames, patchActionName } from '../controllers/action-controllers';
+import { actionValidation } from '../middlewares/action-validator';
 
 const router = Router();
 
-router.post('/new', addAction);
+router.post('/new', actionValidation, addAction);
 router.get('/get/all', getAllActionsNames)
 router.get('/get/:actionName', getActionDetails);
 router.patch('/:actionName', patchActionName);
