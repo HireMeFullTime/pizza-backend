3adf54bd92f693385438d6cb8a8912afb30842b4
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2023-08-11 13:04:45 +0200

    Added  patch name ingredient validation

--- src/routes/ingredient-routes.ts
+++ src/routes/ingredient-routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { addIngredient, deleteIngredient, getAllIngredientsNames, getIngredientDetails, patchIngredientName } from '../controllers/ingredient-controllers';
 import { ingredientValidation } from '../middlewares/ingredient-validator';
+import { patchNameIngredientValidation } from '../middlewares/patch-name-ingredient.validator';
 
 const router = Router();
 
 router.post('/new', ingredientValidation, addIngredient);
 router.get('/get/all', getAllIngredientsNames)
 router.get('/get/:ingredientName', getIngredientDetails);
-router.patch('/:ingredientName', patchIngredientName);
+router.patch('/:ingredientName', patchNameIngredientValidation, patchIngredientName);
 router.delete('/:ingredientName', deleteIngredient)
 
 
